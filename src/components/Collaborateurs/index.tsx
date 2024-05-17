57e3045a96182ef73d03d502739fa769843644f4
1Benjamin-D <desmet.benjamin20@orange.fr>
2024-05-17 15:53:08 +0200

    images and text changes

--- src/components/Collaborateurs/index.tsx
+++ src/components/Collaborateurs/index.tsx
@@ -21,20 +21,22 @@ export default function Collaborateurs() {
       </section>
 
 
-      <section id="collab_1_mobile" className=" flex flex-col items-center gap-[22px] px-[10px] py-5 relative">
+      <section id="collab_1_mobile" className=" flex flex-col items-center gap-[22px] px-[10px] py-5 relative lg:flex-row">
         <div className="flex flex-col gap-[10px] items-center">
           <div className="rounded-full bg-black w-fit">
             <Image src={"/images/logoBP.png"} alt="logo_baptiste_petit" width={55} height={55} className="p-1" />
           </div>
           <h2>Baptiste Petit</h2>
         </div>
-        <p className="w-[300px] text-center">Un développeur web compétent en front et back-end, alliant expertise technique et créativité.</p>
-        <div>
-          <Link href="/nous-contacter" className="flex justify-center items-center w-[343px] h-[41px] bg-custom_yellow rounded-[5px] shadow-lg z-10">
-            Mon Site
-          </Link>
+        <div className="flex flex-col gap-[22px] justify-center items-center">
+          <p className="w-[500px] text-center">Un développeur web compétent en front et back-end, alliant expertise technique et créativité.</p>
+          <div>
+            <Link href="/nous-contacter" className="flex justify-center items-center w-[343px] h-[41px] bg-custom_yellow rounded-[5px] shadow-lg z-10">
+              Mon Site
+            </Link>
+          </div>
         </div>
-        <Image src={"/images/cloud.png"} alt="cloud_logo" width={50} height={50} className=" absolute opacity-10 top-5 right-16 sm:right-32"/>
+        <Image src={"/images/cloud.png"} alt="cloud_logo" width={50} height={50} className=" absolute opacity-10 top-5 right-16 sm:right-32" />
       </section>
 
       <section id="collab_2_mobile" className=" flex flex-col items-center gap-[22px] px-[10px] py-5 relative">
@@ -44,29 +46,29 @@ export default function Collaborateurs() {
           </div>
           <h2>Benjamin Desmet</h2>
         </div>
-        <p className="w-[300px] text-center">Un développeur web compétent en front et back-end, alliant expertise technique et créativité.</p>
+        <p className="w-[500px] text-center">En tant que développeur web expérimenté, j'excelle aussi bien en front-end qu'en back-end, combinant une solide maîtrise technique avec une grande créativité. Je suis prêt à mettre mes compétences à votre disposition pour assurer le succès de votre projet.</p>
         <div>
           <Link href="/nous-contacter" className="flex justify-center items-center w-[343px] h-[41px] bg-custom_yellow rounded-[5px] shadow-lg z-10">
             Mon Site
           </Link>
         </div>
-        <Image src={"/images/planet.png"} alt="cloud_logo" width={50} height={50} className=" absolute opacity-10 top-5 left-16 sm:left-32"/>
+        <Image src={"/images/planet.png"} alt="cloud_logo" width={50} height={50} className=" absolute opacity-10 top-5 left-16 sm:left-32" />
       </section>
 
       <section id="collab_3_mobile" className=" flex flex-col items-center gap-[22px] px-[10px] py-5 relative">
         <div className="flex flex-col gap-[10px] items-center">
           <div className="rounded-full bg-black w-fit">
-            <Image src={"/images/logoBD2.png"} alt="logo_gabin_buignet" width={55} height={55} className="p-1" />
+            <Image src={"/images/logoGB.png"} alt="logo_gabin_buignet" width={55} height={55} className="p-1" />
           </div>
           <h2>Gabin Buignet</h2>
         </div>
-        <p className="w-[300px] text-center">Un développeur web compétent en front et back-end, alliant expertise technique et créativité.</p>
+        <p className="w-[500px] text-center">Passionné par le codage informatique depuis de nombreuses années, j'ai toujours eu une prédilection pour le développement web, tant pour l'aspect design que pour le côté technique. Je serais ravi de mettre mes compétences à votre service pour mener à bien votre projet.</p>
         <div>
           <Link href="/nous-contacter" className="flex justify-center items-center w-[343px] h-[41px] bg-custom_yellow rounded-[5px] shadow-lg z-10">
             Mon Site
           </Link>
         </div>
-        <Image src={"/images/client.png"} alt="cloud_logo" width={50} height={50} className=" absolute opacity-10 top-5 right-16 sm:right-32"/>
+        <Image src={"/images/client.png"} alt="cloud_logo" width={50} height={50} className=" absolute opacity-10 top-5 right-16 sm:right-32" />
       </section>
 
       <section id="collab_4_mobile" className=" flex flex-col items-center gap-[22px] px-[10px] py-5 relative bg-gradient-to-t from-0% from-custom_blue_black to-10% to-white">
@@ -76,13 +78,13 @@ export default function Collaborateurs() {
           </div>
           <h2>Maxime Petit</h2>
         </div>
-        <p className="w-[300px] text-center">Un développeur web compétent en front et back-end, alliant expertise technique et créativité.</p>
+        <p className="w-[500px] text-center">Un développeur web compétent en front et back-end, alliant expertise technique et créativité.</p>
         <div>
           <Link href="/nous-contacter" className="flex justify-center items-center w-[343px] h-[41px] bg-custom_yellow rounded-[5px] shadow-lg z-10">
             Mon Site
           </Link>
         </div>
-        <Image src={"/images/server.png"} alt="cloud_logo" width={50} height={50} className=" absolute opacity-10 top-5 left-16 sm:left-32"/>
+        <Image src={"/images/server.png"} alt="cloud_logo" width={50} height={50} className=" absolute opacity-10 top-5 left-16 sm:left-32" />
       </section>
     </main>
   )
