5c36f67ffe347cc4ff3f91738a9eb57fa410ef1f
1Benjamin-D <desmet.benjamin20@orange.fr>
2024-05-24 13:16:54 +0200

    fix + install framer

--- src/components/Collaborateurs/index.tsx
+++ src/components/Collaborateurs/index.tsx
@@ -6,7 +6,7 @@ export default function Collaborateurs() {
   return (
     <main>
       <section id="intro_collab" className="bg-gradient-to-b from-0% from-custom_blue_black to-10% to-white flex justify-around items-center relative">
-        <Image src={"/images/planet.png"} alt="world_wide_web" width={100} height={100} className="hidden lg:flex opacity-10 w-52 h-52"/>
+        <Image src={"/images/planet.png"} alt="world_wide_web" width={100} height={100} className="hidden lg:flex opacity-10 w-52 h-52" />
         <div className="flex flex-col">
           <div className="flex flex-col items-center gap-[43px]">
             <h1 className="font-bold text-4xl mt-[43px]">Collaborateurs</h1>
@@ -21,7 +21,7 @@ export default function Collaborateurs() {
             </div>
           </div>
         </div>
-        <Image src={"/images/planet.png"} alt="world_wide_web" width={100} height={100} className="hidden lg:flex opacity-10 w-52 h-52"/>
+        <Image src={"/images/planet.png"} alt="world_wide_web" width={100} height={100} className="hidden lg:flex opacity-10 w-52 h-52" />
       </section>
 
       <div className="hidden lg:flex relative">
@@ -61,7 +61,7 @@ export default function Collaborateurs() {
           <p className="w-[300px] md:w-[500px] text-center">En tant que développeur web expérimenté, j'excelle aussi bien en front-end qu'en back-end, combinant une solide maîtrise technique avec une grande créativité. Je suis prêt à mettre mes compétences à votre disposition pour assurer le succès de votre projet.</p>
           <div>
             <Link href="/nous-contacter" className="flex justify-center items-center w-[343px] h-[41px] bg-custom_yellow rounded-[5px] shadow-lg z-10">
-            Portfolio
+              Portfolio
             </Link>
           </div>
         </div>
@@ -84,7 +84,7 @@ export default function Collaborateurs() {
           <p className="w-[300px] md:w-[500px] text-center">Passionné par le codage informatique depuis de nombreuses années, j'ai toujours eu une prédilection pour le développement web, tant pour l'aspect design que pour le côté technique. Je serais ravi de mettre mes compétences à votre service pour mener à bien votre projet.</p>
           <div>
             <Link href="/nous-contacter" className="flex justify-center items-center w-[343px] h-[41px] bg-custom_yellow rounded-[5px] shadow-lg z-10">
-            Portfolio
+              Portfolio
             </Link>
           </div>
         </div>
@@ -107,7 +107,7 @@ export default function Collaborateurs() {
           <p className="w-[300px] md:w-[500px] text-center">Je suis un développeur web spécialisé en front-end et back-end, alliant expertise technique et créativité pour livrer des sites à la fois performants et esthétiquement plaisants. Ma maîtrise de JavaScript, React et Node.js m'aide à trouver des solutions pour correspondre à vos attentes.</p>
           <div>
             <Link href="/nous-contacter" className="flex justify-center items-center w-[343px] h-[41px] bg-custom_yellow rounded-[5px] shadow-lg z-10">
-            Portfolio
+              Portfolio
             </Link>
           </div>
         </div>
